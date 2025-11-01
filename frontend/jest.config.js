2ca964ee2cae22224cbbbe7a8fb046837a9bc028
urk23cs1197 <niransonc@gmail.com>
2025-11-01 22:02:52 +0530

    new commit

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
+  // Transform all JS except for node_modules, but include axios for transformation
   transformIgnorePatterns: [
-    "/node_modules/(?!axios/).+\\.js$"
+    "/node_modules/(?!(axios)/)"
   ],
 };
