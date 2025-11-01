ffd2d9571856c2a25f93b712ca484b737ec87e48
urk23cs1197 <niransonc@gmail.com>
2025-11-01 22:09:58 +0530

    new commit

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders diabetes risk assessment title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByText(/diabetes risk assessment/i);
+  expect(titleElement).toBeInTheDocument();
 });
